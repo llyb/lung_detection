3ca64099176d8d254ec623b5a46367f1bce04324
Ethreal <1615115712@qq.com>
2025-03-04 09:37:58 +0800

    project init

---
+++ backend/lung_detection/src/test/java/com/example/lung_detection/LungDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lung_detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LungDetectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
